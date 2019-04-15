f7ff3a255668cb9bcae36b216003f88483070969
md <matanda1@ac.sce.ac.il>
2019-04-15 16:37:32 +0300

    start main frame

--- src/ui/MainScreen.java
+++ src/ui/MainScreen.java
@@ -7,11 +7,11 @@ public class MainScreen extends JFrame{
     public MainScreen()
     {
         this.setTitle("Competition");
-        JPanel p1 = new ArenaPanel("None");
+        JPanel p1 = new ArenaPanel("Sunny.jpg");
         JPanel p2 = new SidePanel();
         this.setLayout(new BorderLayout());
-        this.getContentPane().add(p1, BorderLayout.NORTH);
-        this.getContentPane().add(p2, BorderLayout.SOUTH);
+        this.getContentPane().add(p1, BorderLayout.WEST);
+        this.getContentPane().add(p2, BorderLayout.EAST);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
     }
--- src/utilities/Program.java
+++ src/utilities/Program.java
@@ -1,30 +1,10 @@
-/**
- *
- */
+
 package utilities;
+import ui.MainScreen;
 
-import game.GameEngine;
-import game.arena.IArena;
-import game.arena.WinterArena;
-import game.competition.Competition;
-import game.competition.Competitor;
-import game.competition.SkiCompetition;
-import game.entities.Entity;
-import game.entities.IMobileEntity;
-import game.entities.MobileEntity;
-import game.entities.sportsman.Skier;
-import game.entities.sportsman.Snowboarder;
-import game.enums.*;
-
-import java.lang.reflect.Constructor;
-
-/**
- * @author Itzhak Eretz Kdosha
- * Main class(run demo)
- * IMPORTANT: getting the same results as published does not mean u have correctly implemented all aspects of this home work!
- * I am avilable for any question via my mail iekdosha@gmail.com
- */
 public class Program {
-
-
+    public static void main(String[] args) {
+        MainScreen frame = new MainScreen();
+        frame.setVisible(true);
+    }
 }
