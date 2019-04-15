3f388252df4c90cc9ffdef3932ab96692527bee2
md <matanda1@ac.sce.ac.il>
2019-04-15 16:22:20 +0300

    start main frame

--- src/ui/MainScreen.java
+++ src/ui/MainScreen.java
@@ -1,7 +1,20 @@
 package ui;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
-public class MainScreen {
-
+public class MainScreen extends JFrame{
+    public MainScreen()
+    {
+        this.setTitle("Competition");
+        JPanel p1 = new ArenaPanel("None");
+        JPanel p2 = new SidePanel();
+        this.setLayout(new BorderLayout());
+        this.getContentPane().add(p1, BorderLayout.NORTH);
+        this.getContentPane().add(p2, BorderLayout.SOUTH);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
     private ArenaPanel arenaPanel;
     private SidePanel sidePanel;
 
