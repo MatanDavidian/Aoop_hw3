6d15152316cc2e554c67848b118276960ac08f2e
md <matanda1@ac.sce.ac.il>
2019-04-17 21:34:11 +0300

    123

--- src/ui/MainScreen.java
+++ src/ui/MainScreen.java
@@ -77,6 +77,55 @@ public class MainScreen extends JFrame{
 
 
 
+
+
+        /**
+         * **************************************
+         * buildArena Button -  action listener *
+         * **************************************
+         */
+        ((SidePanel) sidePanel).createCompetition.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /**
+                 * setting the arena.
+                 */
+                String arenaName = ((SidePanel) sidePanel).getWeatherCondition().getSelectedItem().toString() + ".jpg";
+                String arenaLengthString = ((SidePanel) sidePanel).getArenaLenght().getText().toString();
+                String arenaSnowSurface = ((SidePanel) sidePanel).getSnowSurface().getSelectedItem().toString().toUpperCase();
+                String arenaWeatherCondition = ((SidePanel) sidePanel).getWeatherCondition().getSelectedItem().toString().toUpperCase();
+                int arenaLength = 0;
+                /**
+                 * if arenaLengthString is not int , the value of arenaLength will be zero.
+                 */
+                try {
+                    arenaLength = Integer.parseInt(arenaLengthString);
+                }
+                catch (NumberFormatException ex){
+                    arenaLength = 0;
+                }
+
+
+                try {
+                    //set image according to name and length.
+                    ((ArenaPanel) arenaPanel).setImage(arenaName , arenaLength);
+                }
+                catch (ValueException ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+
+                // adding the arena details to competition
+                arena = new WinterArena(arenaLength , SnowSurface.valueOf(arenaSnowSurface) , WeatherCondition.valueOf(arenaWeatherCondition));
+
+                //print details of the arena.
+                System.out.println(arena);
+
+                //painting the canvas.
+                revalidate();
+                repaint();
+            }
+        });
+
     }
     private ArenaPanel arenaPanel;
     private SidePanel sidePanel;
