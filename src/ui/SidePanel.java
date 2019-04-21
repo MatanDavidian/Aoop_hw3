77524f86c20f3133fc38e30c423c14edc9bbdba6
md <matanda1@ac.sce.ac.il>
2019-04-21 18:52:55 +0300

    123

--- src/ui/SidePanel.java
+++ src/ui/SidePanel.java
@@ -24,7 +24,7 @@ public class SidePanel extends JPanel {
     private JComboBox<String> discipline;
     private JComboBox<String> league;
     private JComboBox<String> gender;
-    private JButton createCompetition;
+     JButton createCompetition;
 
     /**
      * add competitor part
